2c3f726ba9107ceb36aab6eff2ecc4ca42b2f110
Fernando Aponte <fernandoaponte0609@gmail.com>
2024-08-23 23:59:41 -0400

    Feature: PWA Integration & Style: Responsivness Implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,23 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Booking App",
   description: "Your booking experience in Puerto Rico",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["booking", "barbers", "saas", "schedule", "appointment"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    { name: "Digital Sunsets" },
+    {
+      name: "Digital Sunsets",
+      url: "https://www.linkedin.com/in/digital-sunsets/",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon512_rounded.png" },
+    { rel: "icon", url: "icons/icon512_maskable.png" },
+  ],
 };
 
 export default function RootLayout({
